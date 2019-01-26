e34042b1f61f8d3d9cb54b6c75cde9c1456be169
Bekzod <rakhmatovbekzod1996@gmail.com>
2019-01-26 12:47:27 +0900

    UIView Extension has been implemented to be able to use view layouts easlily and stack view has been setup

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+            let view = UIView()
+            view.backgroundColor = color
+            return view
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subviews)
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.fillSuperview()
     }
-
-
 }
 
