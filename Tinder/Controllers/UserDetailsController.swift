3a0832dcd1d602b2a7ce332ab34a7392b476e2fe
Bekzod <rakhmatovbekzod1996@gmail.com>
2019-02-04 12:14:10 +0900

    User Details View Controller Stretchy Header has been implemented on ScrollView

--- Tinder/Controllers/UserDetailsController.swift
+++ Tinder/Controllers/UserDetailsController.swift
@@ -8,9 +8,56 @@
 
 import UIKit
 
-class UserDetailsController: UIViewController {
+class UserDetailsController: UIViewController, UIScrollViewDelegate {
+    
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.alwaysBounceVertical = true
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
+        return scrollView
+    }()
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "lady4c"))
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "User name 30\nDoctor\nSome bio text below"
+        label.numberOfLines = 0
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        view.addSubview(scrollView)
+        scrollView.fillSuperview()
+        
+        scrollView.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width)
+        
+        scrollView.addSubview(infoLabel)
+        infoLabel.anchor(top: imageView.bottomAnchor, leading: scrollView.leadingAnchor, bottom: nil, trailing: scrollView.trailingAnchor, padding: .init(top: 16, left: 16, bottom: -16, right: -16))
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTapGesture)))
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let changeY = -scrollView.contentOffset.y
+        var width = view.frame.width + changeY * 2
+        width = max(view.frame.width, width)
+        imageView.frame = CGRect(x: min(0, -changeY), y: min(0, -changeY), width: width, height: width)
+    }
+    
+    @objc fileprivate func handleTapGesture() {
+        
+        self.dismiss(animated: true, completion: nil)
     }
 }
